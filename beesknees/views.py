84b5cabcc727d434d53b7f49bcff380afbfef935
Luwam Ghebremicael <luwammikael@gmail.com>
2023-07-27 17:08:12 -0700

    creates .gitignore and moves venv into of it, installs dependencies and creates requirements.txt with pip freeze > requirements.txt

--- beesknees/views.py
+++ beesknees/views.py
@@ -5,9 +5,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from django.shortcuts import render
-from django.http import Http404
-
 #get all the exercises
 #serialize them
 #return json
